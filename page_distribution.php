ea8879b6ce55ef025e6718d5b72e6fd105edd5ed
Kat Betz <katbetz@ufl.edu>
2015-10-26 17:24:14 -0400

    updated layout for distribution page

---
+++ page_distribution.php
@@ -0,0 +1,27 @@
+<?php /* Template Name: Distribution */ ?>
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+<!-- BEGIN PAGE PHP -->
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+        			get_template_part( 'content', 'bottompic' );
+
+
+    
+                     
+          
+
+                endwhile;
+                dynamic_sidebar('distribution-widget'); 
+ ?>
+<!-- END PAGE PHP -->
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+
+
+<?php get_footer(); ?>
