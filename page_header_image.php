855e42b41ba55585f23ac82188417c720b0c24d6
Kat Betz <katbetz@ufl.edu>
2015-10-27 15:50:21 -0400

    updated layout for header image

--- page_header_image.php
+++ page_header_image.php
@@ -10,7 +10,7 @@ get_header(); ?>
 
 			// Include the page content template.
             //Content page is pulling in featured image through 		twentyfifteen_post_thumbnail(); needs restyling 
-			get_template_part( 'content', 'headerpic' );
+			get_template_part( 'content', 'nopic' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
