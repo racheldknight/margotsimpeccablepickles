36fd96f4d96a2bb360e3cee4c2aa5d3a2b35597a
Kat Betz <katbetz@ufl.edu>
2015-10-27 15:50:51 -0400

    updated layout for products

--- page_product.php
+++ page_product.php
@@ -21,7 +21,7 @@ get_header(); ?>
 		while ( have_posts() ) : the_post();
 
 			// Include the page content template.
-			get_template_part( 'content', 'bottompic' );
+			get_template_part( 'content', 'nopic' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
@@ -29,12 +29,17 @@ get_header(); ?>
 			endif;
 
 		// End the loop.
-		endwhile;
-		?>
-        </div>
+?>
+				</div>
         <div id="rtpannel">
+	<?php
+		// Post thumbnail.
+		twentyfifteen_post_thumbnail();
+	?>
+	<?php
+                  endwhile;             
+ ?> 
 		<?php dynamic_sidebar( 'product-widget' ); ?>
-        </div>
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
 
