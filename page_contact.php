7f50e12f05c3b316b0b451f1916ece57b1ece90e
Kat Betz <katbetz@ufl.edu>
2015-10-27 15:49:17 -0400

    updated layout for contact

--- page_contact.php
+++ page_contact.php
@@ -3,14 +3,19 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		<div id="ltpannel">
+
 		<?php
 		// Start the loop.
 		while ( have_posts() ) : the_post();
 
+		// Post thumbnail.
+		twentyfifteen_post_thumbnail();
+	?>
+		<div id="ltpannel">
+			<?php
 			// Include the page content template.
             //Content page is pulling in featured image through 		twentyfifteen_post_thumbnail(); needs restyling 
-			get_template_part( 'content', 'headerpic' );
+			get_template_part( 'content', 'nopic' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
